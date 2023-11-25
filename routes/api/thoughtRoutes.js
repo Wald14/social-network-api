a1c69037d0d741f3b23201e45e6748f8c39efb2a
Wald14 <lukewald@hotmail.com>
2023-11-25 17:38:21 -0600

    Removed more unneeded comments

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,31 +1,3 @@
-/*
-  /api/thoughts
-
-  DONE GET to get all thoughts
-
-  DONE GET to get a single thought by its _id
-
-  POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-
-    // example data
-      {
-        "thoughtText": "Here's a cool thought...",
-        "username": "lernantino",
-        "userId": "5edff358a0fcb779aa7b118b"
-      }
-
-  DONE PUT to update a thought by its _id
-
-  DONE DELETE to remove a thought by its _id
-
-*/
-
-
-/*
-  POST to create a reaction stored in a single thought's reaction array field
-
-  DELETE to pull and remove a reaction by the reaction's reactionId value
-*/
 
 const router = require('express').Router();
 
